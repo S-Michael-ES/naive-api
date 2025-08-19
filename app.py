d06714b04162b4b81f82d2953eeb69f6426efa79
S-Michael-ES <S.Michael.ES@protonmail.com>
2025-08-19 13:16:51 -0500

    Modified main script for Heroku compatability

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import psycopg2
+import os
 from flask import Flask, jsonify
 
 DB_NAME = "naive"
@@ -7,6 +8,7 @@ DB_PASS = "14159265"
 DB_HOST = "localhost"
 DB_PORT = "5432"
 
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres://your_user:your_password@localhost:5432/naive')
 app = Flask(__name__)
 
 @app.route('/api/artists')
@@ -14,13 +16,7 @@ def get_artists():
     conn = None
     try:
         # Establish the database connection
-        conn = psycopg2.connect(
-            dbname=DB_NAME,
-            user=DB_USER,
-            password=DB_PASS,
-            host=DB_HOST,
-            port=DB_PORT
-        )
+        conn = psycopg2.connect(DATABASE_URL)
         cur = conn.cursor()
 
         # Execute a query
